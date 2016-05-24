c8e2b9e09ffc5e9f55b6a848497696b795709081
James Delorey <delorey@adobe.com>
2016-05-23 21:44:59 -0600

    initial strings for l10n

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,8 @@ angular.module('semita', []);
 
 angular.module('semita')
 	.controller('MainController', ['$scope', function ($scope) {
-        $scope.greeting = 'test';
-        console.log('this');
-      }]);
+    $scope.stringOne = 'this is string one';
+    $scope.stringTwo = 'this is string two';
+    $scope.stringThree = 'this is string three';
+    $scope.stringFour = 'this is string four';
+  }]);
